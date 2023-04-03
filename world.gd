f368809354eb6d9ce1c1572647b56784ff62c099
Heartbeast <heartbeast.studios@gmail.com>
2023-04-03 15:08:01 -0600

    Texture Button + Signals

--- world.gd
+++ world.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var launch_button = $LaunchButton
+@onready var rocket_flare = $Rocket/RocketFlare
 
-func _ready():
+func _on_launch_button_button_down():
 	animation_player.play("launch")
+	launch_button.hide()
+	rocket_flare.show()
